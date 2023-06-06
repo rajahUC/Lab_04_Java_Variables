efd0c31f4e5332a5b7c3880f905ab6fbc313da1d
rajahUC <rajaho@mail.uc.edu>
2023-06-05 23:11:24 -0400

    All done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 5;
+        int intOperandB = 8;
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        double doubleOperandA = 4.50;
+        double doubleOperandB = 3.20;
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+        double doubleQuotient = 0;
+
+        intSum = intOperandA + intOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intProduct = intOperandA * intOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The product using ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        intDifference = intOperandA - intOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        intQuotient = intOperandA / intOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        intModulo = intOperandA % intOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The remainder using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+        doubleSum = doubleOperandA + doubleOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The sum using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        doubleProduct = doubleOperandA * doubleOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The product using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+        doubleDifference = doubleOperandA - doubleOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The difference using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+        doubleQuotient = doubleOperandA / doubleOperandB; // Assignment that uses the arithmetic operator +
+        System.out.println("The quotient using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+        int numOfKids = 2;
+        boolean isRaining = false;
+        double gasPrice = 3.14;
+        int favNumber = 7;
+        double shoeSize = 9.5;
+        String birthMonth = "July";
+        String fullName = "Husain Raja";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
